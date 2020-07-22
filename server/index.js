a4c020a1dc35fe977e5b7fc1b15e596f948c1a00
khumair <khawja.umair@gmail.com>
2020-07-22 18:13:43 +0300

    Socket io setup on Chat.js page

--- server/index.js
+++ server/index.js
@@ -5,8 +5,21 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
+// const { Socket } = require('socket.io-client');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('we have a new connection !!!');
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+
+
+    });
+    socket.on('disconnect', () => {
+        console.log('User just left !!!');
+    })
+});
 app.use(router);
 server.listen(PORT, () => console.log(`Server has start on port ${PORT}`));
