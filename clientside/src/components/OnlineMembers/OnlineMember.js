ef830ffbe847cae190d333773fa919c1f9b62d49
khumair <khawja.umair@gmail.com>
2020-08-04 13:44:19 +0300

    small fix on text

--- clientside/src/components/OnlineMembers/OnlineMember.js
+++ clientside/src/components/OnlineMembers/OnlineMember.js
@@ -7,8 +7,8 @@ import './OnlineMember.css';
 const OnlineMember = ({ users }) => (
     <div className="textContainer">
         <div>
-            <h1>Realtime Chat Application <span role="img" aria-label="emoji">💬</span></h1>
-            <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji">❤️</span></h2>
+            <h1>Realtime Chat Application <span role="img" aria-label="emoji"></span></h1>
+            <h2>Created with React, Express, Node and Socket.IO <span role="img" aria-label="emoji"></span></h2>
         </div>
         {
             users
